d51ec935d8e655e6becd785f5292b62ff55054ec
TAUKANE PIRES <47633093+taukane@users.noreply.github.com>
2024-04-17 00:55:35 -0300

    Fix
    Fix

--- src/main.jsx
+++ src/main.jsx
@@ -11,15 +11,14 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: "REACT_APP_apiKey",
-	authDomain: "REACT_APP_authDomain",
-	projectId: "REACT_APP_projectId",
-	storageBucket: "REACT_APP_storageBucket",
-	messagingSenderId: "REACT_APP_messagingSenderId",
-	appId: "REACT_APP_appId",
-	measurementId: "REACT_APP_measurementId"
+	apiKey: "AIzaSyD9oaTHGy1QF0aYncLbe4ADpNS1HTr76vw",
+	authDomain: "portfolio-taukane.firebaseapp.com",
+	projectId: "portfolio-taukane",
+	storageBucket: "portfolio-taukane.appspot.com",
+	messagingSenderId: "1037598800771",
+	appId: "1:1037598800771:web:33e3c5d639b347232714b6",
+	measurementId: "G-6L6D6911HD"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
