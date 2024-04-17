4110d50e86997e9206876cf9917ce078f1f3edd6
TAUKANE PIRES <47633093+taukane@users.noreply.github.com>
2024-04-16 23:00:35 -0300

    Fixes
    Clear code React

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-import './App.css'
+import './App.css';
+import  './index.js';
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+import 'react-horizontal-scrolling-menu/dist/styles.css';
 function App() {
 	function dimOn() {
 		document.getElementById("QuickView").style.display = "block";
@@ -18,7 +21,7 @@ function App() {
 					<h1>Designer Curitiba</h1>
 				</a>
 				<div className="caxa">
-					<h2 className="text-secondary">Taukane Portfolio React</h2>
+					<h2 className="text-secondary">Taukane</h2>
 					<h3>Designer Web e Gráfico</h3>
 					<p className="bio-text">
 						Atuo em criação e desenvolvimento de interfaces visuais, para mídias
@@ -28,16 +31,14 @@ function App() {
 						<a
 							href="http://www.linkedin.com/in/taukane"
 							title="linkedin Taukane"
-							target="_blank"
-						>
+							target="_blank">
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
 								width="35"
 								height="35"
 								fill="#0a66c2"
 								className="bi bi-linkedin"
-								viewBox="0 0 16 16"
-							>
+								viewBox="0 0 16 16">
 								<path
 									d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
 							</svg>
@@ -167,114 +168,114 @@ function App() {
 				</div>
 			</div>
 			<div id="QuickView">
-				<div class="coda-slider-wrapper" id="coda-slider-1" data-key="slider">
-					<div class="coda-slider">
-						<div class="panel">
-							<div class="panel-wrapper">
-								<h2 class="title">Volvo CE</h2>
+				<div className="coda-slider-wrapper" id="coda-slider-1" data-key="slider">
+					<div className="coda-slider">
+						<div className="panel">
+							<div className="panel-wrapper">
+								<h2 className="title">Volvo CE</h2>
 								<span>/ Cover Facebook + Email Marketing </span>
 								<img
 									alt="Volvo CE Cover Facebook"
-									class="lazy"
+									className="lazy"
 									src="image/work22_big.jpg"
 									width={1024}
 								/>
 							</div>
 						</div>
-						<div class="panel">
-							<div class="panel-wrapper">
-								<h2 class="title">Marmoraria Florianópolis</h2>
+						<div className="panel">
+							<div className="panel-wrapper">
+								<h2 className="title">Marmoraria Florianópolis</h2>
 								<span>/ Indentidade Visual </span>
 								<img
 									alt="Marmoraria Florianópolis - Redesign de Marca"
-									class="lazy"
+									className="lazy"
 									src="image/marmoraria-florianopolis-2014.jpg"
 									width={1024}
 								/>
 							</div>
 						</div>
-						<div class="panel">
-							<div class="panel-wrapper">
-								<h2 class="title">Probat Leogap</h2>
+						<div className="panel">
+							<div className="panel-wrapper">
+								<h2 className="title">Probat Leogap</h2>
 								<span>/ Website Interface + Desenvolvimento Wordpress</span>
 								<img
 									alt="Criação site Probat Leogap"
 									title="Criação site Probat Leogap"
-									class="image/probat-leogap.jpg"
+									src="image/probat-leogap.jpg"
 									width={1024}
 								/>
 							</div>
 						</div>
-						<div class="panel">
-							<div class="panel-wrapper">
-								<h2 class="title">Curitiba Cultura</h2>
+						<div className="panel">
+							<div className="panel-wrapper">
+								<h2 className="title">Curitiba Cultura</h2>
 								<span>/ Projeto Gráfico Midia Kit</span>
 								<img
 									alt="Curitiba Cultura - Projeto Gráfico"
-									class="lazy"
+									className="lazy"
 									src="image/curitiba-cultura.jpg"
 									width={1024}
 								/>
 							</div>
 						</div>
-						<div class="panel">
-							<div class="panel-wrapper">
-								<h2 class="title">Roldão Atacadista</h2>
+						<div className="panel">
+							<div className="panel-wrapper">
+								<h2 className="title">Roldão Atacadista</h2>
 								<span>/ Facebook Posts</span>
 								<img
 									alt="Facebook Posts"
-									class="lazy"
+									className="lazy"
 									src="image/roldao-posts.jpg"
 									width={1024}
 								/>
 							</div>
 						</div>
-						<div class="panel">
-							<div class="panel-wrapper">
-								<h2 class="title">Zeta Estaleiro</h2>
+						<div className="panel">
+							<div className="panel-wrapper">
+								<h2 className="title">Zeta Estaleiro</h2>
 								<span>
               / Indentidade Visual + Website Interface + Desenvolvimento
               Wordpress{" "}
             </span>
 								<img
 									alt="Zeta Estaleiro - Identidade Corporativa"
-									class="lazy"
+									className="lazy"
 									src="image/zeta-estaleiro-redesign.jpg"
 									width={1024}
 								/>
 							</div>
 						</div>
-						<div class="panel">
-							<div class="panel-wrapper">
-								<h2 class="title">Gazeta do Povo</h2>
+						<div className="panel">
+							<div className="panel-wrapper">
+								<h2 className="title">Gazeta do Povo</h2>
 								<span>/ Interface</span>
 								<img
 									alt="Interface Design"
-									class="lazy"
+									className="lazy"
 									src="image/gazeta.jpg"
 									width={1024}
 								/>
 							</div>
 						</div>
-						<div class="panel">
-							<div class="panel-wrapper">
-								<h2 class="title">Shopping Total</h2>
+						<div className="panel">
+							<div className="panel-wrapper">
+								<h2 className="title">Shopping Total</h2>
 								<span>/ Cover Facebook + Campanha Facebook</span>
 								<img
 									alt="Interface Design"
-									class="lazy"
+									className="lazy"
 									src="image/shopping-total.jpg"
 									width={1024}
 								/>
 							</div>
 						</div>
-						<div class="panel">
-							<div class="panel-wrapper">
-								<h2 class="title">Desenvolvimento Front-End</h2>
+						<div className="panel">
+							<div className="panel-wrapper">
+								<h2 className="title">Desenvolvimento Front-End</h2>
 								<span/>
 								<img
 									alt="Interface Design"
-									class="lazy"
+									className="lazy"
 									src="image/work18_big.jpg"
 									width={1024}
 								/>
@@ -299,3 +300,19 @@ function App() {
 }
 
 export default App
+
+function onWheel(apiObj: scrollVisibilityApiType, ev: React.WheelEvent): void {
+	const isThouchpad = Math.abs(ev.deltaX) !== 0 || Math.abs(ev.deltaY) < 15;
+
+	if (isThouchpad) {
+		ev.stopPropagation();
+		return;
+	}
+
+	if (ev.deltaY < 0) {
+		apiObj.scrollNext();
+	} else if (ev.deltaY > 0) {
+		apiObj.scrollPrev();
+	}
+}
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-function dimOff(){
-    document.getElementById("QuickView").style.display = "none";
-}
-function dimOn(){
-    document.getElementById("QuickView").style.display = "block";
-}
+// $(function() {
+// 	$("img").lazyload({
+// 		event : "sporty"
+// 	});
+// });
+// $(window).bind("load", function() {
+// 	var timeout = setTimeout(function() { $("img").trigger("sporty") }, 700);
+// });
